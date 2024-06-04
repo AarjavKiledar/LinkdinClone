8c92ae949fa76fcd4498121a50ce722774ca4e88
AarjavKiledar <aarjavkiledar@gmail.com>
2024-06-04 12:21:55 +0530

    First Commit

---
+++ components/Widget.tsx
@@ -0,0 +1,16 @@
+function Widget() {
+  return (
+    <div className="ml-6 h-[790px]">
+      <iframe
+        width="100%"
+        height="650"
+        src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=9.755404916951623, 76.6500304372943&amp;q=Indian Institute of Information Technology (IIIT) Kottayam
+        &amp;ie=UTF8&amp;t=&amp;z=13&amp;iwloc=B&amp;output=embed"
+      ></iframe>
+      <br />
+    </div>
+  );
+}
+
+export default Widget;
+  
